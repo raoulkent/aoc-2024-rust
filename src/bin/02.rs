1b0ad4a067c7880e83bcd5e974adfa9bdf4d30dc
Raoul Kent <raoulkent@gmail.com>
2025-03-24 14:51:49 +0100

    complete day 2 part 1

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,11 +1,39 @@
 advent_of_code::solution!(2);
 
 pub fn part_one(input: &str) -> Option<u64> {
+    Some(
+        input
+            .lines()
+            .filter(|line| !line.is_empty())
+            .filter(|line| is_safe(line))
+            .count() as u64,
+    )
+}
+
+pub fn part_two(_input: &str) -> Option<u64> {
     None
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
-    None
+fn is_safe(s: &str) -> bool {
+    let collection: Vec<i32> = s
+        .split_whitespace()
+        .map(|s| s.parse().expect("Failed to parse number"))
+        .collect();
+
+    if collection.len() <= 1 {
+        return true;
+    }
+
+    let is_increasing = collection[1] > collection[0];
+
+    collection.windows(2).all(|w| {
+        let diff = w[1] - w[0];
+        let abs_diff = diff.abs();
+
+        let monotonic_check = if is_increasing { diff > 0 } else { diff < 0 };
+
+        monotonic_check && (1..=3).contains(&abs_diff)
+    })
 }
 
 #[cfg(test)]
