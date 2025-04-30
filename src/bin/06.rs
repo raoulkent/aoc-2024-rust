b3fe416c65cee11e60119f8930e1ecdd50bfd372
Raoul Kent <raoulkent@gmail.com>
2025-04-30 23:04:15 +0200

    wip day 6

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,5 +1,44 @@
 advent_of_code::solution!(6);
 
+fn find_starting_position(input: &str) -> Option<(usize, usize, Direction)> {
+    input.lines().enumerate().find_map(|(y, line)| {
+        line.char_indices()
+            .find_map(|(x, c)| Direction::try_from(c).ok().map(|dir| (x, y, dir)))
+    })
+}
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    fn delta(self) -> (isize, isize) {
+        match self {
+            Direction::Up => (0, -1),
+            Direction::Down => (0, 1),
+            Direction::Left => (-1, 0),
+            Direction::Right => (1, 0),
+        }
+    }
+}
+
+impl TryFrom<char> for Direction {
+    type Error = ();
+
+    fn try_from(c: char) -> Result<Self, Self::Error> {
+        match c {
+            '^' => Ok(Direction::Up),
+            'v' => Ok(Direction::Down),
+            '<' => Ok(Direction::Left),
+            '>' => Ok(Direction::Right),
+            _ => Err(()),
+        }
+    }
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
     None
 }
